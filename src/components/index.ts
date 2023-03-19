2cfca7c01eaa9084821307f329209534f5cdf281
Tadas Šlajus <tlajus@gmail.com>
2023-03-20 00:36:28 +0200

    Implemented LazyLoading wrapper component;

--- src/components/index.ts
+++ src/components/index.ts
@@ -7,3 +7,4 @@ export { default as Logo } from "./Logo/Logo";
 export { default as LearnMore } from "./Buttons/LearnMore";
 export { default as MessageForm } from "./MessageForm/MessageForm";
 export { default as Paragraph } from "./Paragraph/Paragraph";
+export { default as LazyLoading } from "./LazyLoading/LazyLoading";
